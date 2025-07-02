e9022c069c1ade82ae6ce89e5483bfb4f3e0ecbd
ashb155 <b.ashutosh198@gmail.com>
2025-07-02 10:31:04 +0300

    Initial Commit

---
+++ CitiesApp/CitiesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesAppApp.swift
+//  CitiesApp
+//
+//  Created by nbk on 02/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CitiesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CitiesAppTests/CitiesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesAppTests.swift
+//  CitiesAppTests
+//
+//  Created by nbk on 02/07/2025.
+//
+
+import Testing
+@testable import CitiesApp
+
+struct CitiesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
